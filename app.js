70fbc26878db5028c6a574fcd54c28840b2a0a3a
vkhafizov <khafizov.vr@gmail.com>
2025-01-31 23:23:32 +0700

    Update app.js

--- app.js
+++ app.js
@@ -28,4 +28,16 @@ function startGame() {
     }
 }
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+            .then((registration) => {
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            })
+            .catch((err) => {
+                console.log('ServiceWorker registration failed: ', err);
+            });
+    });
+}
+
 startGame();
