fa09f4e51eab91fe9fc6777014955898193fcf05
vkhafizov <khafizov.vr@gmail.com>
2025-01-31 23:22:52 +0700

    Update

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'jerry-games-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/app.js',
+    '/manifest.json',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
